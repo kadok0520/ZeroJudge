718fcc35a0bf6a14530f2910b04bb7eb01d38f30
Sean Lee <leejoneshane@gmail.com>
2017-09-02 10:54:26 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 if [[ "${DB_HOST}" != "mysql_server" ]]; then
   sed -ri \
       -e "s!password=\"(.*)\"!password=\"${DB_PASSWORD}\"!g" \
-      -e "s!url=\"jdbc:mysql://(.*):3306/zerojudge(.*)\"!url=\"jdbc:mysql://${DB_HOST}:3306/zerojudge\"!g" \
       -e "s!connectionPassword=\"(.*)\"!connectionPassword=\"${DB_PASSWORD}\"!g" \
-      -e "s!connectionURL=\"jdbc:mysql://(.*):3306/zerojudge(.*)\"!connectionURL=\"jdbc:mysql://${DB_HOST}:3306/zerojudge\"!g" \
+      -e "s!jdbc:mysql://(.*):3306!jdbc:mysql://${DB_HOST}:3306!g" \
       /usr/local/tomcat/webapps/ROOT/META-INF/context.xml
       
   RESULT=`mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} | grep zerojudge`
   if [ -z "$RESULT" ]; then
-    echo "CREATE DATABASE zerojudge CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "CREATE DATABASE zerojudge CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
     mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" zerojudge < /root/zerojudge.sql
   fi
 fi
