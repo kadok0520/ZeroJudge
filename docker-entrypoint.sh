a973176805e80bdc1428702f6807bd4990a5dd2b
Sean Lee <leejoneshane@gmail.com>
2017-09-02 07:02:23 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,5 @@ if [[ "${DB_HOST}" != "mysql_server" ]]; then
   mysql -u ${DB_USER} -p ${DB_PASSWORD} < /root/zerojudge.sql
 fi
 
+chmod -R 755 /ZeroJudge_CONSOLE /JudgeServer_CONSOLE
 catalina.sh run
