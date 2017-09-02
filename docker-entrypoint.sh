17c7a70d5407b851e5ee2398d2c8e5d3ddd85938
Sean Lee <leejoneshane@gmail.com>
2017-09-02 10:30:39 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,13 @@
 set -euo pipefail
 
 if [[ "${DB_HOST}" != "mysql_server" ]]; then
+  sed -ri \
+      -e "s!password=\"(.*)\"!password=\"${DB_PASSWORD}\"!g" \
+      -e "s!url=\"jdbc:mysql://(.*):3306!url=\"jdbc:mysql://${DB_HOST}:3306!g" \
+      -e "s!connectionPassword=\"(.*)\"!connectionPassword=\"${DB_PASSWORD}\"!g" \
+      -e "s!connectionURL=\"jdbc:mysql://(.*):3306!connectionURL=\"jdbc:mysql://${DB_HOST}:3306!g" \
+      /usr/local/tomcat/webapps/ROOT/META-INF/context.xml
+      
   RESULT=`mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} | grep zerojudge`
   if [ -z "$RESULT" ]; then
     echo "CREATE DATABASE zerojudge CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
