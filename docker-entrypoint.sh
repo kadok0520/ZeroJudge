63e25791029e31cdb9aeda82c18f86b3cf24a9e0
Sean Lee <leejoneshane@gmail.com>
2017-09-02 08:33:02 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
-mysql -h db -u ${DB_USER} -p ${DB_PASSWORD} < /root/zerojudge.sql
+if [[ "${DB_HOST}" != "mysql_server" ]]; then
+  mysql -h ${DB_HOST} -u ${DB_USER} -p ${DB_PASSWORD} < /root/zerojudge.sql
+fi
 
 catalina.sh run
