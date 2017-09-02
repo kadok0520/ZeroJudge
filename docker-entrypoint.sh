f2376e4a6e7c07bea2eb9edaa26e2dacbeddf34f
Sean Lee <leejoneshane@gmail.com>
2017-09-02 08:57:57 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,11 @@
 set -euo pipefail
 
 if [[ "${DB_HOST}" != "mysql_server" ]]; then
-  echo "CREATE DATABASE zerojudge CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
-  mysql --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} zerojudge < /root/zerojudge.sql
+  RESULT=`mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} | grep zerojudge`
+  if [ -z "$RESULT" ]; then
+    echo "CREATE DATABASE zerojudge CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" zerojudge < /root/zerojudge.sql
+  fi
 fi
 
 catalina.sh run
